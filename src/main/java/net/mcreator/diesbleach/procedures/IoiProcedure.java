b0545d9658f631224155cf863ec3e393c6adae19
lead14 <10saiwatabon@gmail.com>
2023-03-31 01:31:13 +0900

    Ioi up

--- src/main/java/net/mcreator/diesbleach/procedures/IoiProcedure.java
+++ src/main/java/net/mcreator/diesbleach/procedures/IoiProcedure.java
@@ -1,48 +1,44 @@
 package net.mcreator.diesbleach.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.world.BlockEvent;
-
+import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
-import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.advancements.Advancement;
 
 import net.mcreator.diesbleach.DiesbleachMod;
 
 import java.util.Map;
-import java.util.HashMap;
 
 public class IoiProcedure {
-	@Mod.EventBusSubscriber
-	private static class GlobalTrigger {
-		@SubscribeEvent
-		public static void onBlockBreak(BlockEvent.BreakEvent event) {
-			Entity entity = event.getPlayer();
-			IWorld world = event.getWorld();
-			Map<String, Object> dependencies = new HashMap<>();
-			dependencies.put("xpAmount", event.getExpToDrop());
-			dependencies.put("x", event.getPos().getX());
-			dependencies.put("y", event.getPos().getY());
-			dependencies.put("z", event.getPos().getZ());
-			dependencies.put("px", entity.getPosX());
-			dependencies.put("py", entity.getPosY());
-			dependencies.put("pz", entity.getPosZ());
-			dependencies.put("world", world);
-			dependencies.put("entity", entity);
-			dependencies.put("blockstate", event.getState());
-			dependencies.put("event", event);
-			executeProcedure(dependencies);
-		}
-	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				DiesbleachMod.LOGGER.warn("Failed to load dependency world for procedure Ioi!");
+			return;
+		}
+		if (dependencies.get("advancement") == null) {
+			if (!dependencies.containsKey("advancement"))
+				DiesbleachMod.LOGGER.warn("Failed to load dependency advancement for procedure Ioi!");
+			return;
+		}
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
 				DiesbleachMod.LOGGER.warn("Failed to load dependency entity for procedure Ioi!");
 			return;
 		}
+		IWorld world = (IWorld) dependencies.get("world");
+		Advancement advancement = (Advancement) dependencies.get("advancement");
 		Entity entity = (Entity) dependencies.get("entity");
-		entity.attackEntityFrom(DamageSource.GENERIC, (float) 1);
+		if (world instanceof World && ((World) world).getServer() != null
+				? ((World) world).getServer().getAdvancementManager().getAdvancement(new ResourceLocation("none")).equals(advancement)
+				: false) {
+			if (entity instanceof PlayerEntity) {
+				((PlayerEntity) entity).abilities.isFlying = (true);
+				((PlayerEntity) entity).sendPlayerAbilities();
+			}
+		}
 	}
 }
